04ef914f6efed7427c15d8308e7b5f8dce76b25d
Enache Ionut <enachestoianionut@gmail.com>
2018-09-18 10:54:42 +0300

    include general settings in the new options reprezentation model

---
+++ ClangPowerTools/ClangPowerTools/Options/Model/ClangSettings.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClangPowerTools.Options.Model
+{
+  [Serializable]
+  public class ClangSettings
+  {
+
+    #region Properties
+
+
+    #region General Properties
+
+    public List<string> ClangFlags { get; set; } = new List<string>();
+
+    public List<string> FilesToIgnore { get; set; } = new List<string>();
+
+    public List<string> ProjectsToIgnore { get; set; } = new List<string>();
+
+    public ClangGeneralAdditionalIncludes? TreatAdditionalIncludesAs { get; set; }
+
+    public bool TreatWarningsAsErrors { get; set; }
+
+    public bool Continue { get; set; }
+
+    public bool ClangCompileAfterVsCompile { get; set; }
+
+    public bool VerboseMode { get; set; }
+
+    public string Version { get; set; }
+
+
+    #endregion
+
+
+    #region Tidy Options Properties
+
+
+
+
+    #endregion
+
+
+    #endregion
+
+
+  }
+}
