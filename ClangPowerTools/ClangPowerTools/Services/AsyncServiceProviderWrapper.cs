5de038de8c45edfe4fc19bb6cde0aa2e9562c9d9
Enache Ionut <enachestoianionut@gmail.com>
2018-09-25 16:42:55 +0300

    implement generic class service provider

---
+++ ClangPowerTools/ClangPowerTools/Services/AsyncServiceProviderWrapper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System.Threading;
+
+namespace ClangPowerTools.Services
+{
+  public class AsyncServiceProviderWrapper<TService> where TService : class
+  {
+    #region Members
+
+    private IAsyncServiceProvider mServiceProvider;
+
+    #endregion
+
+
+    #region Constructor 
+
+    public AsyncServiceProviderWrapper(IAsyncServiceProvider aAsyncServiceProvider)
+      => mServiceProvider = aAsyncServiceProvider;
+
+    #endregion
+
+
+    #region IEnvDTEService implementation
+
+    public async System.Threading.Tasks.Task<TService> GetServiceAsync()
+    {
+      
+      return (TService)await mServiceProvider.GetServiceAsync(typeof(TService));
+    }
+
+    #endregion
+
+  }
+}
